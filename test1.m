a69a7095515578c8496580f57f76eff2cf186e48
YasinZhang <271155397@qq.com>
2019-07-30 14:17:57 +0800

    delete useless file

--- test1.m
+++
@@ -1,7 +0,0 @@
-clear all; clc
-x = [1E-03,1E+00,1E+03,1E+06,1E+09,1E+12,1E+15,1E+18];
-y = [1E-03,1E+00,1E+03,1E+06,1E+09,1E+12,1E+15,1E+18];
-loglog(x,y);
-ylabel('Final Cumulative Wealth');
-set(gca,'Ytick',[1E-03,1E+00,1E+03,1E+06,1E+09,1E+12,1E+15,1E+18]);
-set(gca,'Yticklabel',{'1E-03','1E+00','1E+03','1E+06','1E+09','1E+12', '1E+15','1E+18'});
