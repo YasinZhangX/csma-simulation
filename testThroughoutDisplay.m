aca81d432d63b991c326b3f1b978c86eca326f13
YasinZhang <271155397@qq.com>
2019-07-31 12:25:36 +0800

    change test

--- testThroughoutDisplay.m
+++ testThroughoutDisplay.m
@@ -1,16 +1,16 @@
 figure(3);
-plot(1:n, Throughout(:,:,1), '-*');
+plot(1:n, Throughout(:,:,1), '-*', 'LineWidth',1);
 hold on;
-plot(1:n, Throughout(:,:,2), '-o');
+plot(1:n, Throughout(:,:,2), '-o', 'LineWidth',1);
 hold on;
-plot(1:n, Throughout(:,:,3), '-+');
+plot(1:n, Throughout(:,:,3), '-+', 'LineWidth',1);
 hold on;
-plot(1:n, Throughout(:,:,4), '-d');
+plot(1:n, Throughout(:,:,4), '-d', 'LineWidth',1);
 legend('maxBE=4','maxBE=5','maxBE=6','maxBE=7');
 box on;
 grid on;
 xlabel('Nodes');
-ylabel('Throughout');
+ylabel('Throughout/Number of packets');
 % xlim([1, 15]);                                  % 只设定x轴的绘制范围
 set(gca,'XTick',0:1:n);                          % 改变x轴坐标间隔显示 这里间隔为1
 title('CSMA/CA: Throughout vs Nodes');
